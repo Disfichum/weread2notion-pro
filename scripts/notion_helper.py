9c0c55d05a912d9199c3f52a0a2f1fa18a7d60e0
malinkang <malinkang@adsconflux.com>
2024-04-28 11:26:14 +0800

    fix heatmap

--- scripts/notion_helper.py
+++ scripts/notion_helper.py
@@ -116,7 +116,7 @@ class NotionHelper:
                     child.get("id")
                 )
             elif child["type"] == "embed" and child.get("embed").get("url"):
-                if child.get("url").startswith("https://heatmap.malinkang.com/"):
+                if child.get("embed").get("url").startswith("https://heatmap.malinkang.com/"):
                     self.heatmap_block_id = child.get("id")
             # 如果子块有子块，递归调用函数
             if "has_children" in child and child["has_children"]:
